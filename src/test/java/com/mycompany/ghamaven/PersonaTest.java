7d40579365a381edeb679856037d8fadf7816eda
Pablo Rubio Amondarain <pablorubioamodnarain@gmail.com>
2021-11-25 09:42:43 +0100

    Added Persona and Persona Test

--- src/test/java/com/mycompany/ghamaven/PersonaTest.java
+++ src/test/java/com/mycompany/ghamaven/PersonaTest.java
@@ -75,12 +75,12 @@ public class PersonaTest {
     @org.junit.jupiter.api.Test
     public void testGetId() {
         System.out.println("getId");
-        Persona instance = null;
-        String expResult = "";
+        Persona instance = new Persona("a","b",2);
+        String expResult = "a";
         String result = instance.getId();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        //fail("The test case is a prototype.");
     }
 
     /**
