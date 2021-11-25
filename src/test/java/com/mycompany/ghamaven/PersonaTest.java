31c9a4badcabbe67b8a213e3a199e8b3998abd7c
Pablo Rubio Amondarain <pablorubioamodnarain@gmail.com>
2021-11-25 10:39:34 +0100

    Created Persona and PersonaTest

--- src/test/java/com/mycompany/ghamaven/PersonaTest.java
+++ src/test/java/com/mycompany/ghamaven/PersonaTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import java.util.Random;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -59,13 +60,25 @@ public class PersonaTest {
     @org.junit.jupiter.api.Test
     public void testHBirthday() {
         System.out.println("hBirthday");
-        Persona instance = new Persona("a","b",15,false);
+        
+        Random random = new Random();
+        int initAge = random.nextInt();
+        
+        Persona instance = new Persona("a","b",initAge,false);
         instance.hBirthday();
+        int expResultD=initAge;
+        int resultD = instance.getAge();
         // TODO review the generated test code and remove the default call to fail.
-        if(instance.getAge()!=15)
-        {
-            fail("The test case is a prototype.");
-        }
+        
+        instance.setAlive(true);
+        instance.hBirthday();
+        
+        int expResultA=initAge + 1;
+        int resultA = instance.getAge();        
+        
+        assertEquals(expResultD, resultD);
+        assertEquals(expResultA, resultA);
+
 
     }
 
@@ -81,87 +94,6 @@ public class PersonaTest {
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setId method, of class Persona.
-     */
-    @org.junit.jupiter.api.Test
-    public void testSetId() {
-        System.out.println("setId");
-        String id = "";
-        Persona instance = null;
-        instance.setId(id);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getName method, of class Persona.
-     */
-    @org.junit.jupiter.api.Test
-    public void testGetName() {
-        System.out.println("getName");
-        Persona instance = null;
-        String expResult = "";
-        String result = instance.getName();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setName method, of class Persona.
-     */
-    @org.junit.jupiter.api.Test
-    public void testSetName() {
-        System.out.println("setName");
-        String name = "";
-        Persona instance = null;
-        instance.setName(name);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getAge method, of class Persona.
-     */
-    @org.junit.jupiter.api.Test
-    public void testGetAge() {
-        System.out.println("getAge");
-        Persona instance = null;
-        int expResult = 0;
-        int result = instance.getAge();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of isAlive method, of class Persona.
-     */
-    @org.junit.jupiter.api.Test
-    public void testIsAlive() {
-        System.out.println("isAlive");
-        Persona instance = null;
-        boolean expResult = false;
-        boolean result = instance.isAlive();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setAlive method, of class Persona.
-     */
-    @org.junit.jupiter.api.Test
-    public void testSetAlive() {
-        System.out.println("setAlive");
-        boolean alive = false;
-        Persona instance = null;
-        instance.setAlive(alive);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
